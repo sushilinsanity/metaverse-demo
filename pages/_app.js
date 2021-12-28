071011019d1c1d9a4233ce7b5dc04c5caea2210d
Sushil <sushilmangal731@gmail.com>
2021-12-28 18:36:15 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { MoralisProvider } from 'react-moralis';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId="uX9fwOl6UpooQpjd9IrQi1vzy8QnvQBvXYwrTsDv"
+      serverUrl="https://kmyaurhaqwwr.usemoralis.com:2053/server"
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
